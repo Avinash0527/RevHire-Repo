00b9667ebd3755969f5b9a9a8656fdbe3b647ddf
Avinash <166938611+Avinash0527@users.noreply.github.com>
2026-03-12 01:35:30 +0530

    Update API URL for production environment

--- RevHire-HiringPlatform--Frontend/src/environments/environment.prod.ts
+++ RevHire-HiringPlatform--Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:9090/api' // Change this for production deployment
+    apiUrl: 'http://3.136.233.53:9090/api' // Change this for production deployment
 };
